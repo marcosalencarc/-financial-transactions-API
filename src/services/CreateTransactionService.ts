3566d6c0b5c2ecd4f80a0fa676978df23c2400ba
Marcos de Alencar Carvalho <marcoscarvalho1997@gmail.com>
2020-04-29 15:29:47 -0300

    Implementation of services, routes and repositories

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestTO {
+  title: string;
+  value: number;
+  type: string;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestTO): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('This type of transaction is invalid');
+    }
+    if (type === 'outcome') {
+      const { total } = this.transactionsRepository.getBalance();
+      if (total < value) {
+        throw Error('The value is less than total income');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
